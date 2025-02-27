6b94dca920b46fc109e8256ed228484cb8c873a4
emreaatmacaa <yemre.atmaca@icloud.com>
2025-02-27 23:24:42 +0300

    jenkins

--- src/test/java/tests/JenkinsTest.java
+++ src/test/java/tests/JenkinsTest.java
@@ -46,10 +46,4 @@ public class JenkinsTest {
 	}
 
 }
-echo "# Jenkins" >> README.md
-git init
-git add README.md
-git commit -m "ilk commit"
-git branch -M main
-git remote add origin https://github.com/emreaatmacaa/Jenkins.git
-git push -u origin main
+
