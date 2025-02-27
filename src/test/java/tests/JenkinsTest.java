b46c630f2663e28f3a1ae8919600d4b8f2a42b94
emreaatmacaa <yemre.atmaca@icloud.com>
2025-02-27 23:20:39 +0300

    ilk commit

---
+++ src/test/java/tests/JenkinsTest.java
@@ -0,0 +1,55 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class JenkinsTest {
+
+	@Test
+	void Test1(){
+		assertTrue(true);
+	}
+	@Test
+	void Test2(){
+		assertTrue(true);
+	}
+	@Test
+	void Test3(){
+		assertTrue(true);
+	}
+	@Test
+	void Test4(){
+		assertTrue(true);
+	}
+	@Test
+	void Test5(){
+		assertTrue(true);
+	}
+	@Test
+	void Test6(){
+		assertTrue(true);
+	}
+	@Test
+	void Test7(){ assertTrue(true);}
+	@Test
+	void Test8(){
+		assertTrue(true);
+	}
+	@Test
+	void Test9(){
+		assertTrue(true);
+	}
+	@Test
+	void Test10(){
+		assertTrue(true);
+	}
+
+}
+echo "# Jenkins" >> README.md
+git init
+git add README.md
+git commit -m "ilk commit"
+git branch -M main
+git remote add origin https://github.com/emreaatmacaa/Jenkins.git
+git push -u origin main
